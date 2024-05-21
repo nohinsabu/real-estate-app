e8b0ba3c869a29423fce85458b8b4505393f3e2e
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-05-21 15:46:30 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,68 @@
 import { useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase.js'
 
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user)
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (file) {
+      handlFileUpload(file)
+    }
+  }, [file]);
+
+  const handlFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress))
+      },
+    (error) => {
+      setFileUploadError(true);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadUrl) => 
+          setFormData({...formData, avatar:downloadUrl})
+      );
+    }
+  );  
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='font-semibold text-3xl text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img className='rounded-full h-24 w-24 object-cover 
-        cursor-pointer self-center mt-2' src={currentUser.avatar} alt='Profile Picture'/>
+        <input 
+        onChange={(e) => setFile(e.target.files[0])}
+        type='file' 
+        ref={fileRef} 
+        hidden accept='image/*'/>
+        <img onClick={() => fileRef.current.click()} className='rounded-full h-24 w-24 object-cover 
+        cursor-pointer self-center mt-2' src={formData.avatar || currentUser.avatar} alt='Profile Picture'/>
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>Error uploading image(Image must be less than 2MB)</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image uploaded successfully!</span>
+          ) : (
+            ''
+          )
+        }
+        </p>
         <input type='text' placeholder='Username' id='username'
         className='border p-3 rounded-lg' />
         <input type='text' placeholder='Email' id='Email'
@@ -24,3 +79,10 @@ export default function Profile() {
     </div>
   )
 }
+
+
+//firebase storage
+// allow read;
+//       allow write: if
+//       request.resource.size < 2 * 1024 * 1024 &&
+//       request.resource.contentType.matches('image/.*')
