311aebabad1c50d44f1a50161d4447c4aa6e7802
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-05-23 17:20:07 +0530

    complete create listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -199,7 +199,7 @@ export default function CreateListing() {
                         onChange={handleChange} value={formData.regularPrice}/>
                         <div className='flex flex-col items-center'>
                             <p>Regular price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.type === 'rent' && (<span className='text-xs'>($ / month)</span>)}
                         </div>
                     </div>
                     {formData.offer && (
@@ -209,7 +209,7 @@ export default function CreateListing() {
                         onChange={handleChange} value={formData.discountPrice} />
                         <div className='flex flex-col items-center'>
                             <p>Discounted price</p>
-                            <span className='text-xs'>($ / month)</span>
+                            {formData.type === 'rent' && (<span className='text-xs'>($ / month)</span>)}
                         </div>
                     </div>
                     )}
