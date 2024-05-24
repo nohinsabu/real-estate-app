1785c9d418cdb83e1ce8b8169bd1bd3646862bf2
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-05-24 15:18:21 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,7 +14,7 @@ export const deleteListing = async (req,res,next) => {
     const listing = await Listing.findById(req.params.id);
 
     if(!listing) {
-        return next(errorHandler(401, 'Listing not found!'));
+        return next(errorHandler(404, 'Listing not found!'));
     }
 
     if(req.user.id !== listing.userRef) {
@@ -24,6 +24,27 @@ export const deleteListing = async (req,res,next) => {
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Listing has been deleted!')
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing)
     } catch (error) {
         next(error);
     }
