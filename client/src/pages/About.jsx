05c56d10755cd87796e1f3e8c281b9f5f4d36d99
nohinsabu01 <nohin.sabu@intern.mhtechin.com>
2024-05-29 19:17:03 +0530

    change regarding logo in about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>
-        About NoahEstate</h1>
+        About <span className='text-slate-500'>Noah</span>Estate</h1>
       <p className='mb-4 text-slate-700'>NoahEstate is a leading real 
       estate agency that specializes in helping clients buy, sell, and 
       rent properties in the most desirable neighborhoods. Our team of 
